4448d8b5c25c222b7df0858a3ec48daae340a129
Miguetorresp <miguetorres30@gmail.com>
2023-04-13 22:09:06 -0500

    Exercise solution

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Clock from './components/exercise/Clock';
 import { ClockF } from './components/exercise/ClockF';
+import { ClockS } from './components/exercise/ClockS';
+
 
 function App() {
   return (
@@ -9,7 +11,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {/* <Clock></Clock> */}
-        <ClockF></ClockF>
+        {/* <ClockF></ClockF> */}
+        <ClockS></ClockS>
       </header>
     </div>
   );
